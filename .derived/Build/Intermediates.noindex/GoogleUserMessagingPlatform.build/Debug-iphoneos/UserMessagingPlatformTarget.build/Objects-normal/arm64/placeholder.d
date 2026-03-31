e2c975c756f17f3e921977b172ea31e8e0faf13e
Mouniim <mouniim@192.168.1.112>
2026-04-01 00:40:24 +0100

    Remove large build artifacts

--- .derived/Build/Intermediates.noindex/GoogleUserMessagingPlatform.build/Debug-iphoneos/UserMessagingPlatformTarget.build/Objects-normal/arm64/placeholder.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/mouniim/develop/ios\ projects/DarkAura/Dark\ Aura\ XIT\ Panel/.derived/SourcePackages/checkouts/swift-package-manager-google-user-messaging-platform/UserMessagingPlatformTarget/placeholder.m
